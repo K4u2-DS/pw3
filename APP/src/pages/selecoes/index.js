e2d0455c65ed6b478f99d33b747494f85d648218
K4u2-DS <kauadiscor@gmail.com>
2026-03-19 11:07:52 +0000

    Aula_04

---
+++ APP/src/pages/selecoes/index.js
@@ -0,0 +1,34 @@
+import { getSelecoes } from "../../services/selecoes.service.js";
+
+const container = document.getElementById("selecoes-container");
+
+const renderSelecoes = (selecoes) => {
+    container.innerHTML = "";
+
+    selecoes.forEach((selecao) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${selecao.logo}" />
+            <h2>${selecao.nome}</h2>
+            <p>Grupo: ${selecao.grupo}</p>
+            <p>🏆 Títulos: ${selecao.conquistas.length}</p>
+            <p>👥 Jogadores: ${selecao.jogadores.length}</p>
+        `;
+
+        container.appendChild(card);
+    });
+};
+
+const load = async () => {
+    try {
+        const selecoes = await getSelecoes();
+        renderSelecoes(selecoes);
+    } catch (error) {
+        container.innerHTML = "<p>Erro ao carregar seleções</p>";
+        console.error(error);
+    }
+};
+
+load();
