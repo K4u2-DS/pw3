4a24c5735009055e87a5b8e53f3ff28f6dc80b72
K4u2-DS <kauadiscor@gmail.com>
2026-03-19 11:36:40 +0000

    final aula_04

--- APP/src/config/env.js
+++ APP/src/config/env.js
@@ -1,5 +1,5 @@
-const dominio = "http://localhost";
+const dominio = "https://laughing-couscous-r469g5qx59743jr7-3000.app.github.dev/";
 const port = "3000";
-const baseUrl = `${dominio}:${port}`;
+const baseUrl = `https://laughing-couscous-r469g5qx59743jr7-3000.app.github.dev/`;
 
 export { baseUrl };
