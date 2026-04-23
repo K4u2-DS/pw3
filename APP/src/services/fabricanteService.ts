521810c53cf36806944b2914e5753adb13579b81
K4u2-DS <kauadiscor@gmail.com>
2026-04-23 10:18:46 +0000

    final aula 07

--- APP/src/services/fabricanteService.ts
+++ APP/src/services/fabricanteService.ts
@@ -4,7 +4,7 @@ import type { Fabricante } from "../types/fabricante";
 
 //colocar a url da api, e deixar ela publica
 
-const API_URL = "https://urban-spork-6956gxqpxqx5h5q7x-3000.app.github.dev/fabricantes";
+const API_URL = "https://improved-space-guide-v6r4vj95j9v9fxxp5-3000.app.github.dev/fabricantes";
 
 export async function getFabricante(): Promise<Fabricante[]> {
   const response = await fetch(API_URL);
